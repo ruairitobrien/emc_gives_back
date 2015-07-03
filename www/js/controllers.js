94a4bced51b91f74ef2d28d2ed04c06923ad7609
Steve Riordan <steve.riordan@emc.com>
2015-07-03 13:13:13 +0100

    adding Tasks service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('HealthCtrl', function($scope) {
+.controller('HealthCtrl', function($scope,Tasks) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,30 +11,7 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-        $scope.washTasks = [{
-                img: 'img/health/washing/how_to/tap.png',
-                audio: 'audio/health/washing/how_to/TurnTap.mp3'
-            },
-            {
-                img: 'img/health/washing/how_to/wet.png',
-                audio: 'audio/health/washing/how_to/WetHands.mp3'
-            },
-            {
-                img: 'img/health/washing/how_to/soap.png',
-                audio: 'audio/health/washing/how_to/GetSoap.mp3'
-            },
-            {
-                img: 'img/health/washing/how_to/rub.png',
-                audio: 'audio/health/washing/how_to/RubHands.mp3'
-            },
-            {
-                img: 'img/health/washing/how_to/rinse.png',
-                audio: 'audio/health/washing/how_to/RinseHands.mp3'
-            },
-            {
-                img: 'img/health/washing/how_to/dry.png',
-                audio: 'audio/health/washing/how_to/DryHands.mp3'
-            }];
+        $scope.washTasks =Tasks.washTasks();
 
   
 })
