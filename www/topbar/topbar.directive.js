b22d39348f9477f074b3ef1eaa34202adef2413f
unknown <potocj@IRMFPOTOCJD1C.corp.emc.com>
2015-07-03 12:09:53 +0100

    Adding initial topbar template

--- www/topbar/topbar.directive.js
+++ www/topbar/topbar.directive.js
@@ -1,12 +1,41 @@
 (function () {
     'use strict';
-    angular.module('sarter.topbar').directive('topBar', topBar);
+    angular.module('starter.topbar', ['ionic'])
+    	.directive('topBar', topBar)
+    	.controller('TopbarCtrl', ['$scope', '$ionicModal', topbarCtrl]);
 
 
-    topBar.$inject = [];
+    topBar.$inject = ['$ionicModal'];
 
     function topBar() {
-
+    	return {
+    		'restrict': 'E',
+    		'templateUrl': 'topbar/topbar-template.html',
+    		'controller': 'TopbarCtrl',
+    		'controllerAs': 'topbarCtrl'
+    	}
     }
 
+    function topbarCtrl($scope, $ionicModal){
+    	this.changeColour = function(){
+    		/*$ionicModal.fromTemplateUrl('topbar/topbar.colorPicker.html', {
+    			scope: $scope,
+			    animation: 'slide-in-up'
+			  }).then(function(modal) {
+			    $scope.modal = modal;
+			  }).then(function(){
+			  	$scope.modal.show();
+			  });
+			  $scope.openModal = function() {
+			    $scope.modal.show();
+			  };
+			  $scope.closeModal = function() {
+			    $scope.modal.hide();
+			  };*/
+    	/*this.showBack = false;
+    	this.home = function(){
+    		this.showBack = true;
+    	}*/
+    	}
+    }
 })();
