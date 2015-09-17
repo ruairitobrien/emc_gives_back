933048891785a31e53abeff637959a2968600c77
Sarah O'Brien <sarah.obrien@emc.com>
2015-09-17 08:48:33 +0100

    Tidy up

--- www/src/categories/categories.service.js
+++ www/src/categories/categories.service.js
@@ -4,38 +4,44 @@
     angular.module('givesBack.categories')
         .factory('CategoryService', CategoryService);
 
-    CategoryService.$inject = ['lodash','dpd'];
+    CategoryService.$inject = ['dpd'];
 
     /* @ngInject */
-    function CategoryService(lodash,dpd) {
+    function CategoryService(dpd) {
+
+        var setCategories = function(d){
+            this.categories = d;
+            };
+
+        var getCategories = function(){
+            var me = this;
+            dpd.categories.get().success(function(res){
+                me.setCategories(res);
+            });
+        };
+
+        var createCategory = function(data){
+            var me = this;
+            dpd.categories.post(data).success(function(){
+                //update categories obj
+                me.getCategories();
+            });
+        };
+
+        var removeCategory = function(d){
+            var me = this;
+            dpd.categories.del(d).success(function(){
+                //update categories obj
+                me.getCategories();
+            });
+        };
 
         return {
-
             categories: {},
-
-            getCategories: function(){
-                var me = this;
-                dpd.categories.get().success(function(res){
-                    me.setCategories(res);
-                });
-            },
-            setCategories: function(d){
-                this.categories = d;
-            },
-            createCategory: function(data){
-                var me = this;
-                dpd.categories.post(data).success(function(){
-                    //update categories obj
-                    me.getCategories();
-                });
-            },
-            removeCategory: function(d){
-                var me = this;
-                dpd.categories.del(d).success(function(){
-                    //update categories obj
-                    me.getCategories();
-                });
-            }
+            getCategories: getCategories,
+            createCategory: createCategory,
+            removeCategory: removeCategory,
+            setCategories: setCategories
         };
     }
 
